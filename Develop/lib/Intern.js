60f80d65ef26cc8ee72fcbb35cedc540cc2d6129
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2020-12-05 19:15:26 -0700

    intern test passed

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+// Inheritance with class syntax 
+class Intern extends Employee {
+    constructor(name, id, email, school) {
+      super(name, id, email); //passing parameters from parent
+      this.school = school;    
+    }
+
+    getSchool() {
+        return this.school
+    }
+
+    getRole() {
+        return "Intern"
+
+}
+}
+
+//export class   
+module.exports = Intern
