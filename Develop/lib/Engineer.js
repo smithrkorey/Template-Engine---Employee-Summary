e47ed2823e2677e250b4aa91e78080241ae5e6a4
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2020-12-05 19:09:56 -0700

    engineer test pass

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("../lib/Employee");
+
+// Inheritance with class syntax 
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+      super(name, id, email); //passing parameters from parent
+      this.github = github;    
+    }
+
+    getGithub() {
+        return this.github
+    }
+
+    getRole() {
+        return "Engineer"
+
+}
+}
+
+//export class   
+module.exports = Engineer   
+
+
+
+
+
+
