d3ce4d9f8e0b791fd873c6b85f7fd1ff5e107adc
Your Name <koreysmith@Koreys-MacBook-Pro.local>
2020-12-06 20:02:13 -0700

    app.js edits

--- Develop/app.js
+++ Develop/app.js
@@ -5,82 +5,186 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 // Questions
-const inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
 
-  ])
-  .then(answers => {
-    // instances of each role
-    
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
+const teamMembers = []
+
+function createTeam() {
+
+    inquirer
+        .prompt([
+
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "What is your role?",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern",
+                    "No more employees"
+                ]
+            }
+
+        ]).then(userChoice => {
+            // pass in the variable
+            switch (userChoice.memberChoice) {
+                // in case userChoice
+                case "Manager":
+                    addManager();
+                    break;
+
+                case "Engineer":
+                    addEngineer();
+                    break;
+
+                case "Intern":
+                    addIntern();
+                    break;
+
+                case "No more employees":
+                    render(teamMembers);
+                    break
+
+            }
+        })
+
+
+    function addManager() {
+
+        inquirer
+            .prompt([
+
+                {
+                    type: "input",
+                    message: "What is your name?",
+                    name: "managerName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "managerID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "managerEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "managerOfficeNumber"
+                }
+
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const manager = new Manager(userChoice.managerName, userChoice.managerID, userChoice.managerEmail, userChoice.managerOfficeNumber)
+
+                teamMembers.push(manager)
+
+                createTeam();
+
+            })
+
+
     }
-  });
+
+
+    function addEngineer() {
+        inquirer
+            .prompt([
+
+                {
+                    type: "input",
+                    message: "What is your name?",
+                    name: "engineerName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "engineerID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "engineerEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your GitHub username?",
+                    name: "gitHub"
+                }
+
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const engineer = new Engineer(userChoice.engineerName, userChoice.engineerID, userChoice.engineerEmail, userChoice.engineerGithub)
+
+                teamMembers.push(engineer)
+
+                createTeam();
+
+            })
+    }
 
 
 
 
-// Require/import the HTTP module
-const http = require('http');
+    function addIntern() {
+
+        inquirer
+            .prompt([
+
+                {
+                    type: "input",
+                    message: "What is your name?",
+                    name: "internName"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your employee ID?",
+                    name: "internID"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "internEmail"
+                },
+
+                {
+                    type: "input",
+                    message: "What is your school?",
+                    name: "internSchool"
+                }
+            ]).then(userChoice => {
+                console.log(userChoice);
+
+                const intern = new Intern(userChoice.internName, userChoice.internID, userChoice.internEmail, userChoice.internSchool)
+
+                teamMembers.push(intern)
+
+                createTeam();
+            })
+    }
+}
+
+module.exports = teamMembers
+
+createTeam();
+
 
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
-
-// Define a port to listen for incoming requests
-const PORT = 8080;
-
-// Create a generic function to handle requests and responses
-const handleRequest = (request, response) => {
-  // Send the below string to the client when the user visits the PORT URL
-  response.end(`It Works!! Path Hit: ${request.url}`);
-};
-
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-const server = http.createServer(handleRequest);
-
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, () => {
-  // Log (server-side) when our server has started
-  console.log(`Server listening on: http://localhost:${PORT}`);
-});
-
-
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
